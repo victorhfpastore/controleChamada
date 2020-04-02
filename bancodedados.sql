0e3545ca2b63d5fa386a44ec2ee4aef4819a5485
Victor Pastore <44670369+victorhfpastore@users.noreply.github.com>
2020-04-02 20:35:44 -0300

    Add files via upload

---
+++ bancodedados.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `aula_ps2`.`frequencia` (
+  `idaluno` INT NOT NULL AUTO_INCREMENT,
+  `tia` BIGINT NOT NULL,
+  `entrada` VARCHAR(100) NULL,
+  `saida` VARCHAR(100) NULL,
+  PRIMARY KEY (`idaluno`));
